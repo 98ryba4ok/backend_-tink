cd36290804a607e07b062f8f007d7fa53b45d542
Egor Linnikov <98ryba4ok@MacBook-Pro-Egor-2.local>
2026-05-12 01:02:21 +0300

    fix render deploy

--- start.sh
+++ start.sh
@@ -3,4 +3,7 @@ python manage.py makemigrations movies users events recommendations
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py createsuperuser --no-input || true
+if [ "$SYNC_TMDB_ON_START" = "true" ]; then
+  python manage.py sync_tmdb --pages 10
+fi
 uvicorn cinefind.asgi:application --host 0.0.0.0 --port $PORT
