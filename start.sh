c50361b877bf3b34b3aa6fd42ec2d055285e09a7
Egor Linnikov <98ryba4ok@MacBook-Pro-Egor-2.local>
2026-05-12 00:56:56 +0300

    fix render deploy

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+python manage.py makemigrations movies users events recommendations
 python manage.py migrate
 python manage.py collectstatic --noinput
+python manage.py createsuperuser --no-input || true
 uvicorn cinefind.asgi:application --host 0.0.0.0 --port $PORT
